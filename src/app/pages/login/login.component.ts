af5188c4069b73556ff92bb1a828debfb25f233e
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-06-12 13:36:47 +0200

    edited error msg for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { EmailValidator, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  form: FormGroup;
+  submitted: boolean = false;
 
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  passwordOnFocus(inputElement: HTMLInputElement, imgElement: HTMLImageElement) {
+    this.updateIcon(inputElement, imgElement);
+  }
+
+
+  passwordNotOnFocus(inputElement: HTMLInputElement, imgElement: HTMLImageElement) {
+    if (inputElement.value.length === 0) {
+      imgElement.src = '../../assets/img/login-signup/lock.svg';
+    }
+  }
+
+
+  togglePassword(inputElement: HTMLInputElement, imgElement: HTMLImageElement) {
+    inputElement.type = inputElement.type === 'password' ? 'text' : 'password';
+    this.updateIcon(inputElement, imgElement);
+  }
+
+
+  updateIcon(inputElement: HTMLInputElement, imgElement: HTMLImageElement) {
+    if (inputElement.type === 'password') {
+      imgElement.src = '/assets/img/icons/visibility_off.svg';
+      imgElement.alt = 'Password Visibility Off';
+    } else {
+      imgElement.src = '/assets/img/icons/visibility.svg';
+      imgElement.alt = 'Password Visibility On';
+    }
+  }
+
+  login() {
+    this.submitted = true
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,8 +9,7 @@ export class ApiService {
   private _isError = new BehaviorSubject<boolean>(false);
   isError$ = this._isError.asObservable();
 
-  constructor() { 
-    this._isError.next(true)
+  constructor() {
   }
 
   setIsError(status: boolean) {
